f3848784d0b2d7516694c68dd9f34e33b5182072
Jason Ji <uberjason@gmail.com>
2019-12-17 20:24:27 -0500

    Initial Commit

---
+++ ModulatedPublisher/ModulatedPublisher.h
@@ -0,0 +1,19 @@
+//
+//  ModulatedPublisher.h
+//  ModulatedPublisher
+//
+//  Created by Jason Ji on 12/17/19.
+//  Copyright © 2019 Jason Ji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModulatedPublisher.
+FOUNDATION_EXPORT double ModulatedPublisherVersionNumber;
+
+//! Project version string for ModulatedPublisher.
+FOUNDATION_EXPORT const unsigned char ModulatedPublisherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModulatedPublisher/PublicHeader.h>
+
+
